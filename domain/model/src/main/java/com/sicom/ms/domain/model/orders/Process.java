6bf75fd03d849a0ee2181d452525f948c1e197ab
Jorge Luis Rodriguez Carreño <jorge.rodriguez@sofka.com.co>
2022-02-25 10:12:39 -0500

    ops response improved

--- domain/model/src/main/java/com/sicom/ms/domain/model/orders/Process.java
+++ domain/model/src/main/java/com/sicom/ms/domain/model/orders/Process.java
@@ -1,60 +1,7 @@
 package com.sicom.ms.domain.model.orders;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Process {
     public String processId;
     public String processRadNumber;
-    public SavedMessage savedMessage;
-    @JsonProperty("ProcessGuid")
-    public String processGuid;
-    public ProcessWorkflowClass processWorkflowClass;
     public ProcessError processError;
-    @JsonProperty("CurrentWorkItems")
-    public CurrentWorkItems currentWorkItems;
-}
-
-class SavedMessage{
-    @JsonProperty("Entities")
-    public String entities;
-}
-
-class ProcessWorkflowClass{
-    public String workflowClassId;
-    public String workflowClassName;
-    public String workflowClassDisplayName;
-    public String workflowClassDescription;
-    public String workflowClassHelpText;
-    public String workflowClassDisplayOrder;
-    public String workflowClassCreationDate;
-    public String workflowClassGlobalForm;
-    public String workflowClassAllocationPrinciple;
-    public String workflowClassProcessType;
-    public String workflowClassDisplay;
-    public String workflowClassUseParentRadicationNumber;
-}
-
-class CurrentWorkItems{
-    public WorkItem workItem;
-}
-
-class WorkItem{
-    public String workItemId;
-    public String workItemState;
-    public String workItemEntryDate;
-    public String workItemDuration;
-    public String workItemEstimatedSolutionDate;
-    public Task task;
-}
-
-class Task{
-    public String taskId;
-    public String taskName;
-    public String taskDisplayName;
-    public String taskDescription;
-    public String taskHelpText;
-    public String taskEstimatedDuration;
-    public String taskType;
-    public String taskCost;
-    public String taskPriority;
 }
