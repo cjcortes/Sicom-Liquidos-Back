bb7062bac1e5ecf85e8b04438c1411c7240ec52e
Jorge Luis Rodriguez Carreño <jorge.rodriguez@sofka.com.co>
2022-06-28 09:31:13 -0500

    minor update

--- infrastructure/security/src/main/java/com/sicom/ms/infrastructure/security/SecurityConfiguration.java
+++ infrastructure/security/src/main/java/com/sicom/ms/infrastructure/security/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                         "/api/security/loginns",
                         "/api/two-factor/generate-secret-code",
                         "/api/two-factor/confirm-secret-code",
-                        "/api/two-factor/register-notification-oauth2-webapp",
+                        "/api/two-factor/register-notification-oauth2-webapp"
                 )
                 .permitAll()
                 .anyExchange().authenticated()
