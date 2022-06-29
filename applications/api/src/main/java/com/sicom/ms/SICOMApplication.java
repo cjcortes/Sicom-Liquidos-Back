640a8f6d1d50abb225010d680b705c8e68d9b954
Jorge Luis Rodriguez Carreño <jorge.rodriguez@sofka.com.co>
2022-06-29 11:05:55 -0500

    setup timezone

--- applications/api/src/main/java/com/sicom/ms/SICOMApplication.java
+++ applications/api/src/main/java/com/sicom/ms/SICOMApplication.java
@@ -1,11 +1,22 @@
 package com.sicom.ms;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SICOMApplication {
+    @Value("${spring.application.timezone}")
+    private String timeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(SICOMApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
     }
 }
