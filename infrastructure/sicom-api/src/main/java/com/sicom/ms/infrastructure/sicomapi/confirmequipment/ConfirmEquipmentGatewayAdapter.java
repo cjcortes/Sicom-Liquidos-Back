6c97bcebf034fdb91d474b36cecdbf5f2e2a5e96
pablo.echeverri <pablo.echeverri@sofka.com.co>
2023-02-09 15:28:17 -0500

    - solve memory issue

--- infrastructure/sicom-api/src/main/java/com/sicom/ms/infrastructure/sicomapi/confirmequipment/ConfirmEquipmentGatewayAdapter.java
+++ infrastructure/sicom-api/src/main/java/com/sicom/ms/infrastructure/sicomapi/confirmequipment/ConfirmEquipmentGatewayAdapter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -24,6 +25,11 @@ public class ConfirmEquipmentGatewayAdapter implements ConfirmEquipmentGateway {
                 .baseUrl(baseUrl)
                 .defaultHeaders(header -> header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON)))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(16 * 1024 * 1024))
+                        .build())
                 .build();
 
         return client.post()
